18c0f655dd1fda04046d63b22e2a4e3296c69b60
Miguel <UO247134@uniovi.es>
2019-03-21 12:34:15 +0100

    SDI-NodeJS- 11,12,13,14 Vistas y motores de plantillas

--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -1,10 +1,4 @@
-module.exports = function(app) {
-    app.get("/usuarios", function(req, res) {
-        res.send("ver usuarios");
-    });
-};
-
-module.exports = function(app) {
+module.exports = function(app,swig) {
     app.get("/usuarios", function(req, res) {
         res.send("ver usuarios");
     });
