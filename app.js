80554aced9e9b98043540eb21a24ab7375057965
Miguel <UO247134@uniovi.es>
2019-03-21 11:14:50 +0100

    “SDI-NodeJS- 8,9,10 Recursos estáticos, Peticiones POST y parámetros, Enrutamiento y comodines

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // Módulos
 var express = require('express');
 var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Variables
 app.use(express.static('public'));
+
+// Variables
+app.set('port', 8081);
 
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
